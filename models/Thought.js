6fd860fdf0cf82ed7a20fe2c8d1e8758a29af609
NadeemTalaat <nadeem.talaat@gmail.com>
2023-07-08 15:24:08 -0400

    Define thought.js model

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    // TODO: ASK ABOUT WHY THIS IS REQUIRED
+    type: Schema.Types.ObjectId,
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: () => {
+      const date = this;
+      return date.toLocaleString("en-us");
+    },
+  },
+});
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
